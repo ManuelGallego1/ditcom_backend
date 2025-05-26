bc5cc5ce2d285a0e6bbf580628eae65bfbf55ee3
ManuelGallego1 <manuela.gallegoa@autonoma.edu.co>
2025-05-26 18:10:18 -0500

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,11 +8,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Cors
 {
+    protected $allowedOrigins = [
+        'http://localhost:3000',
+        'https://ditcomfrontend.amcdevcode.com',
+    ];
+
     public function handle(Request $request, Closure $next): Response
     {
+        $origin = $request->headers->get('Origin');
+
+        $allowedOrigin = in_array($origin, $this->allowedOrigins) ? $origin : '';
+
         if ($request->getMethod() === "OPTIONS") {
             return response('', 204)
-                ->header('Access-Control-Allow-Origin', 'http://localhost:3002')
+                ->header('Access-Control-Allow-Origin', $allowedOrigin)
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
                 ->header('Access-Control-Allow-Credentials', 'true');
@@ -20,10 +29,12 @@ class Cors
 
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3002');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        if ($allowedOrigin) {
+            $response->headers->set('Access-Control-Allow-Origin', $allowedOrigin);
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        }
 
         return $response;
     }
